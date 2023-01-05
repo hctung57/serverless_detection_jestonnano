9a8aa6c486aed329299f9ab5f1064b6719a108b6
hctung57 <manhtung05072001@gmail.com>
2023-01-05 21:38:20 +0700

    run

---
+++ test.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+import time
+app = Flask(__name__)
+
+@app.route('/api/test', methods=['GET'])
+def handle_streaming_thread_init():
+    time.sleep(5)
+    return 'OK', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
